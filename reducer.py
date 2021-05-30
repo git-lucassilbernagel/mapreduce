17fd9f177450ea760c0a7f75e41739b0ff03cd52
git-lucassilbernagel <77398086+git-lucassilbernagel@users.noreply.github.com>
2021-05-30 11:11:44 +0100

    6. Task

--- reducer.py
+++ reducer.py
@@ -20,7 +20,8 @@ import sys
 # Cash  455.51
 
 # Sum of all sales (values) is initialized with zero, we just started
-count_of_values = 0
+sum_of_values = 0
+number_iterations = 0
 
 # Previous key is initialized with None, we just started
 previous_key = None
@@ -45,20 +46,21 @@ for line in sys.stdin:
         # to the standart output (stdout)
         # Key and value are seperated by a tab (\t)
         # Line ends with new line (\n)
-        sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
+        sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
         # Sum of sales starts again with 0
-        count_of_values = 0
+        sum_of_values = 0
+        number_iterations = 0
 
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
-    count_of_values += 1
+    sum_of_values += float(value)
+    number_iterations += 1
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
 # write the last result to stdout
-if count_of_values > 114:
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
-else: 
-    pass
+average_of_values = sum_of_values/number_iterations
+sys.stdout.write("{0}\t{1}\n".format(previous_key, average_of_values))
+
